a896479aed3573c50f72b69605601648a20b8e90
Marat Valeev <valeeva.la.1986@gmail.com>
2021-06-03 16:56:59 +0500

    done 'iterators' task

--- iterators.py
+++ iterators.py
@@ -1,5 +1,7 @@
-# 1. Написать класс итератора, который по каждой стране из файла countries.json ищет страницу из википедии.
-# Записывает в файл пару: страна – ссылка. Ссылку формировать самостоятельно.
+# 1. Написать класс итератора, который по каждой стране
+# из файла countries.json ищет страницу из википедии.
+# Записывает в файл пару: страна – ссылка.
+# Ссылку формировать самостоятельно.
 
 import json
 
@@ -8,18 +10,24 @@ PATH_FILE = 'countries.json'
 
 class CountryWikiLinks:
 
-    def __iter__(self):
-        with open(PATH_FILE, encoding="utf-8") as f:
-            json_data = json.load(f)
-        for item in json_data:
-            self.countries = iter([country for country in item['name']['common']])
+    def __init__(self, path):
+        self.path = path
 
-        return self
+    def __iter__(self):
+        with open(self.path, encoding="utf-8") as f:
+            json_data = json.load(f)
+
+        with open('countries.txt', 'w', encoding='utf-8') as f:
+            for item in json_data:
+                self.countries = item['name']['common']
+                self.links = 'https://en.wikipedia.org/wiki/' + str(self.countries).replace(' ', '_')
+                yield self.links
+                f.write(f'{self.countries} - {self.links}\n')
 
     def __next__(self):
-        country = next(self.countries)
-        yield country.text
+        link = next(self.links)
+        return link
 
 
-for country in CountryWikiLinks():
-    print(country)
+for links in CountryWikiLinks(PATH_FILE):
+    pass
